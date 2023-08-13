983fe55343bc5a4948bad759e939ebe1c6a21abc
renato1958 <felisi.renato@gmail.com>
2023-08-13 06:13:29 -0400

    Creato router ed importato nel file principale

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import { connectToDB } from "./config/database.js";
+import authRouter from "./routes/api/auth.js";
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/auth", authRouter);
 
 const startServer = (async () => {
     try {

7b74c3079a5751ae67bb3d3cd51073c8938882c3
renato1958 <felisi.renato@gmail.com>
2023-08-13 06:33:13 -0400

    Create le principali routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,4 +3,9 @@ import * as authController from "../../controllers/authControllers.js";
 
 const router = express.Router();
 
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.post("/refresh", authController.refresh);
+
 export default router;
